da08f9160629a110e182456f75575715f74b63d1
Anassaouji <aoujianass@gmail.com>
2024-04-20 16:33:40 +0100

    role and permission last

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -10,7 +10,11 @@ class Customer extends Model
     use HasFactory;
 public function user(){
     return $this->belongsTo(User::class);
-    
+
+}
+public function adresse(){
+    return $this->hasOne(Adresse::class);
+
 }
 
 }
